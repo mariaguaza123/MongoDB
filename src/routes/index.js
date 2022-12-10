409783c177e954ad851d31873ebce283d5d35266
mariaguaza123 <mafe199912@hotmail.com>
2022-12-09 19:12:02 -0500

    Se añade la base de datos con MongoDB

---
+++ src/routes/index.js
@@ -0,0 +1,27 @@
+const { Router } = require('express');
+const productsRouter = require('./products');
+const cartRouter = require('./cart');
+const productsRouterDB = require('./productsMongoDB');
+
+
+// Conexion a la base de datos
+const mongoose = require('mongoose');
+const user = 'root';
+const password = 'mafe';
+const bd = 'myeccomerce';
+const uri = `mongodb+srv://root:${password}@cluster0.cvm762g.mongodb.net/${bd}?retryWrites=true&w=majority`;
+mongoose.set('strictQuery', true);
+mongoose.connect(uri)
+  .then(() => console.log('Connected!'))
+  .catch(e => console.log(e));
+
+
+
+//Rutas del proyecto
+const routerMain = Router();
+routerMain.use('/products', productsRouter);
+routerMain.use('/cart', cartRouter);
+//Routers desde la base de datos
+routerMain.use('/productsDB', productsRouterDB);
+
+module.exports = routerMain;
